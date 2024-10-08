dbaa7f8de5f92b634eb891af9153262b64f7b12e
Daniel Zarate <danzarate902@gmail.com>
2024-10-08 17:58:39 -0500

    Index.htl Actualizacion

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Flask via Gunicorn and Nginx!"
+def home():
+    try:
+        return render_template("index.html")  # Renderiza index.html
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 if __name__ == '__main__':
     app.run()
